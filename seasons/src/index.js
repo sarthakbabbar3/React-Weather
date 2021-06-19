4a65e6cff2c584d2a583b8a67fb6292eaec4d1ee
sarthak babbar <sarthakbabbar3@gmail.com>
2021-06-20 00:42:36 +0530

    Added season icons and season and error css files

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,34 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import "./LocationError.css";
 
 class App extends React.Component {
-  // overriding constructor
-  constructor(props) {
-    super(props); //parent constructor
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
+      return (
+        <div className="location-error">
+          <i className="massive location arrow icon" />
+          <div>Error: {this.state.errorMessage}</div>
+        </div>
+      );
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat} </div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
 }
 
